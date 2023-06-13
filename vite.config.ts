74646286a6ae825f63da06c1253c86e5a68706e1
zypherscript <kitti.kthp@gmail.com>
2023-06-13 14:19:46 +0700

    vitetest + playwright

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 /// <reference types="vitest" />
-import { defineConfig } from 'vite'
+import { configDefaults, defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
@@ -9,5 +9,6 @@ export default defineConfig({
     environment: 'jsdom',
     setupFiles: './tests/setup.ts',
     css: false,
+    exclude: [...configDefaults.exclude, './tests/playwright/*'],
   },
 })
